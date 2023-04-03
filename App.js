e6fd3ca493684e1c31c5f2f0f2597665b687d455
poornimavishnu <poornimavishnukumar11@gmail.com>
2023-04-03 11:45:33 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GOOD MORNING</h1>
+        <button onClick={window['Poori']}>press button</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
